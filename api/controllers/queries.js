34cd831345fb75b04a3bcdb47f474283a245bb9b
JacobCromwell <thecoldsleep9@gmail.com>
2020-02-03 22:26:02 -0500

    first pass at createItem

--- api/controllers/queries.js
+++ api/controllers/queries.js
@@ -10,14 +10,36 @@ const pool = new Pool({
 console.log('pool: ', pool);
 
 const getUsers = (request, response) => {
-    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
-    })
-  }
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const createItem = (request, response) => {
+  /*
+  TODOs
+    * pass in listID <- needs to be done via the BE side
+  */
+  //console.log('request.body: ', request.body);
+
+  let { list_id, url, img_url, purchased, title, description } = request.body
+  let create_date = new Date(Date.now()).toLocaleString();
+  let update_date = new Date(Date.now()).toLocaleString();
+
+  pool.query('INSERT INTO item (list_id, url, img_url, purchased, title, description, create_date, update_date) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', 
+  [list_id, url, img_url, purchased, title, description, create_date, update_date], (error, results) => {
+    if (error) {
+      throw error
+    }
+    console.log('results: \n', results);
+    response.status(201).send(`Item added with ID: ${results.insertId}`)
+  })
+}
 
 module.exports = {
-    getUsers
+  getUsers,
+  createItem
 }
--- api/index.js
+++ api/index.js
@@ -1,23 +1,3 @@
-/*
-var express = require('express');
-var app = express();
-let helloWorld  = require('./controllers/helloWorld.js');
-
-app.route('/Hello').get(function(req,res) {
-    let helloWorldMsg = helloWorld.data.getMsg();
-    console.log('Msg: ', helloWorldMsg);
-    res.send(helloWorldMsg);
-});
-app.route('/status').get(function(req,res)
-{
-    res.send('List Helper Service is running\nWe do not know how well');
-});
-app.get('/',(function(req,res){
-    res.send('List Helper Service is running\nWe do not know how well');
-}));
-
-var server=app.listen(3000,function() {});
-*/
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
@@ -54,8 +34,9 @@ app.get('/status', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
 });
 
+app.get('/users', db.getUsers)
+app.post('/item', db.createItem)
+
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
 });
-
-app.get('/users', db.getUsers)
